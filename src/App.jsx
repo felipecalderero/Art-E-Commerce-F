a82e9022d44169677a882b4c206f2ddc4976c67d
felipecalderero <felipecalderero@gmail.com>
2024-04-12 14:18:50 +0200

    basic app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import NotFoundPage from "./pages/NotFoundPage";
+import AllPaintingsPage from "./pages/AllPaintingsPage";
+import AboutPage from "./pages/AboutPage";
+//import Navbar from "/src/components/Navbar.jsx";
+//import Footer from "/src/components/Footer.jsx";
 
 function App() {
   return (
-    <>
-      <Navbar></Navbar>
-    </>
+    <div className="App">
+      <h1>Art E-Commerce</h1>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<AllPaintingsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+
+        {/* <Route path="/arts/:artId" element={<PaintingsDetailsPage />} />
+  <Route path="/users/:userId" element={<ArtistDetailsPage />} /> */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      {/* <Footer /> */}
+    </div>
   );
 }
 
